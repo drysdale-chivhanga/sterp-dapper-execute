86c022144501eb5ba601bf5f05496566fef87c4c
Drysdale Chivhanga <chivrys@live.com>
2023-04-08 12:04:53 +0200

    first commit

---
+++ IDataExecute.cs
@@ -0,0 +1,27 @@
+﻿namespace Sterp.DapperExecute;
+
+/// <summary>
+/// Interface.Data.Execute
+/// </summary>
+public interface IDataExecute
+{
+    /// <summary>
+    /// GetData
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="storedProcedure"></param>
+    /// <param name="parameters"></param>
+    /// <param name="connectionString"></param>
+    /// <returns></returns>
+    Task<IEnumerable<T>> GetData<T, U>(string storedProcedure, U parameters, string connectionString = "DefaultCon");
+    /// <summary>
+    /// SaveData
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="storedProcedure"></param>
+    /// <param name="parameters"></param>
+    /// <param name="connectionString"></param>
+    /// <returns></returns>
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionString = "DefaultCon");
+}
---
+++ MySqlDataExecute.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using MySqlConnector;
+using System.Data;
+
+namespace Sterp.DapperExecute;
+/// <summary>
+/// MySql.Data.Execute
+/// </summary>
+public class MySqlDataExecute : IDataExecute
+{
+
+    /// <summary>
+    /// GetData
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="storedProcedure"></param>
+    /// <param name="parameters"></param>
+    /// <param name="connectionString"></param>
+    /// <returns>List</returns>
+    public async Task<IEnumerable<T>> GetData<T, U>(string storedProcedure, U parameters, string connectionString = "DefaultCon")
+    {
+        using MySqlConnection connection = new(connectionString);
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    /// <summary>
+    /// SaveData
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="storedProcedure"></param>
+    /// <param name="parameters"></param>
+    /// <param name="connectionString"></param>
+    /// <returns>Void</returns>
+    public async Task SaveData<T>(string storedProcedure, T parameters, string connectionString = "DefaultCon")
+    {
+        using MySqlConnection connection = new(connectionString);
+        await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
